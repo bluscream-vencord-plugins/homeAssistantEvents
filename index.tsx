576392317ce12afbfe57ee80f2136d7a086118d5
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:53:13 +0100

    Refactor: Adhered to Gold Standard conventions

--- index.tsx
+++ index.tsx
@@ -14,17 +14,8 @@ import { settings } from "./settings";
 // endregion Imports
 
 // region PluginInfo
-export const pluginInfo = {
-    id: "homeAssistantEvents",
-    name: "HomeAssistantEvents",
-    description: "Sends Discord events to HomeAssistant for automation",
-    color: "#00bcd4",
-    authors: [
-        { name: "Bluscream", id: 467777925790564352n },
-        { name: "Assistant", id: 0n }
-    ],
-};
-// endregion PluginInfo
+import { pluginInfo } from "./info";
+export { pluginInfo };
 
 // region Variables
 const logger = new Logger(pluginInfo.id, pluginInfo.color);
---
+++ info.ts
@@ -0,0 +1,10 @@
+export const pluginInfo = {
+    id: "homeAssistantEvents",
+    name: "HomeAssistantEvents",
+    description: "Sends Discord events to HomeAssistant for automation",
+    color: "#00bcd4",
+    authors: [
+        { name: "Bluscream", id: 467777925790564352n },
+        { name: "Assistant", id: 0n }
+    ],
+};
